3fd3b225c46ecda8b6d828916584134a9a4e4eaf
Angel Gonzalez <agb92@live.com>
2021-02-04 22:25:36 -0600

    Eventos y Formularios en HTML y JavaScript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,28 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#faa";
-
-// while (l < lineas) {
-//   yi = 10 * l;
-//   xf = 10 * (l + 1);
-//   dibujarLinea(colorcito, 0, yi, xf, 300);
-//   console.log("lineas " + l);
-//   // l = l + 1;
-//   l++;
-// }
-
-for (l = 0; l < lineas; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(colorcito, 0, yi, xf, 300);
-  console.log("lineas " + l);
-}
-
-dibujarLinea("blue", 0, 0, 0, 300);
-dibujarLinea("blue", 0, 300, 300, 300);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath(); //inicia
@@ -31,4 +13,34 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.lineTo(xfinal, yfinal); //crear linea
   lienzo.stroke(); // cerrar la linea
   lienzo.closePath(); // termnina
+}
+
+function dibujoPorClick() {
+  // var x = texto.value;
+  // alert("hola mundo " + x);
+  // var xxx = parseInt(texto.value);
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#faa";
+  var espacio = ancho / lineas;
+  // while (l < lineas) {
+  //   yi = 10 * l;
+  //   xf = 10 * (l + 1);
+  //   dibujarLinea(colorcito, 0, yi, xf, 300);
+  //   console.log("lineas " + l);
+  //   // l = l + 1;
+  //   l++;
+  // }
+
+  for (l = 0; l < lineas; l++) {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("lineas " + l);
+  }
+  // dibujarLinea("blue", 0, 0, 0, 300);
+  // dibujarLinea("blue", 0, 300, 300, 300);
+  dibujarLinea("blue", 0, 0, 0, ancho - 1);
+  dibujarLinea("blue", 0, ancho - 1, ancho - 1, ancho - 1);
 }
