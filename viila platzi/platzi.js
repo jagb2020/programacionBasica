2ede8bcef1d806f441e920dd8ef4ee723803daa9
Angel Gonzalez <agb92@live.com>
2021-02-06 21:58:13 -0600

    Funciones matemáticas y números aleatorios en JavaScript

---
+++ viila platzi/platzi.js
@@ -0,0 +1,14 @@
+var z;
+// var z = aleatorio(10, 20);
+// document.write(z);
+
+for (var i = 0; i < 10; i++) {
+  z = aleatorio(10, 20);
+  document.write(z + ", ");
+}
+
+function aleatorio(min, max) {
+  var resultado;
+  resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado;
+}
