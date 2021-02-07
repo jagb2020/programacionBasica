6e82c3263073d7b87cfc15a1b2109d164290267d
Angel Gonzalez <agb92@live.com>
2021-02-06 19:07:42 -0600

    Dibujar en canvas con las flechas del teclado

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -8,6 +8,22 @@ var teclas = {
 console.log(teclas);
 
 document.addEventListener("keydown", dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", 149, 149, 151, 151, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
+  lienzo.beginPath(); //inicia
+  lienzo.strokeStyle = color; //color de la linea
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial); //moverse al punto en X punto en Y
+  lienzo.lineTo(xfinal, yfinal); //crear linea
+  lienzo.stroke(); // cerrar la linea
+  lienzo.closePath(); // termnina
+}
 
 function dibujarTeclado(evento) {
   // if (evento.keyCode == teclas.UP) {
@@ -16,18 +32,24 @@ function dibujarTeclado(evento) {
   // if (evento.keyCode == teclas.DOWN) {
   //   console.log("abajo");
   // }
+  var colorcito = "blue";
+  var movimiento = 10;
   switch (evento.keyCode) {
     case teclas.UP:
-      console.log("arriba");
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
       break;
     case teclas.DOWN:
-      console.log("abajo");
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
       break;
     case teclas.LEFT:
-      console.log("izquierda");
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
       break;
     case teclas.RIGHT:
-      console.log("derecha");
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
       break;
     default:
       console.log("otra tecla");
