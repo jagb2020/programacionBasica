d6d9411e6a734cd83d8d959ead9415552d71ca81
Angel Gonzalez <agb92@live.com>
2021-02-19 23:07:33 -0600

    Clases y Arrays en JavaScript

---
+++ Pakiman/paki.js
@@ -0,0 +1,32 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+// var cauchin = new Pakiman("Cauchin", 100, 30);
+// var pokacho = new Pakiman("Pokacho", 80, 50);
+// var tocinauro = new Pakiman("Tocinauro", 120, 40);
+
+// console.log(Cauchin);
+// console.log(Pokacho);
+// console.log(Tocinauro);
+// console.log(Cauchin, Pokacho, Tocinauro);
+
+// pokacho.hablar();
+
+// cauchin.mostrar();
+// pokacho.mostrar();
+// tocinauro.mostrar();
+
+var coleccion = [];
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
+
+for (var pakin of coleccion) {
+  pakin.mostrar();
+}
+
+for (var imagenes in imagenes) {
+  console.log(imagenes);
+}
